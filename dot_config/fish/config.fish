8f7d28eac2a2b6a52e52f61ce3c140fe1beaffaa
BlockLune <39331194+BlockLune@users.noreply.github.com>
2023-10-18 21:00:43 +0800

    fix: fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,6 +6,5 @@ thefuck --alias | source  # thefuck required
 zoxide init fish | source  # zoxide required
 
 alias bat='batcat'  # bat required, only on ubuntu
-alias cd='z'  # zoxide required
 alias hx='helix' # helix required, only on archlinux
 alias ll='lsd -l'  # lsd required
