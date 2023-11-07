c2dc44ce081c0e9b2e949e45b492e7f280c41cf7
BlockLune <39331194+BlockLune@users.noreply.github.com>
2023-11-07 17:24:05 +0800

    fix: aliases based on the operating system

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,6 +5,32 @@ end
 thefuck --alias | source  # thefuck required
 zoxide init fish | source  # zoxide required
 
-alias bat='batcat'  # bat required, only on ubuntu
-alias hx='helix' # helix required, only on archlinux
-alias ll='lsd -l'  # lsd required
+# Function: SetupAliases
+# Description: Sets up aliases based on the operating system defined in /etc/os-release.
+function setup_aliases
+    # Sets up aliases which work on every system
+    alias ll='lsd -l'  # lsd required
+
+    # Read the /etc/os-release file
+    set -gx os_release (cat /etc/os-release)
+
+    # Parse the contents of the os_release variable
+    set -l ID (string match 'ID=*' $os_release)
+    set -l VERSION_ID (string match 'VERSION_ID=*' $os_release)
+
+    # Extract the values from the matches
+    set -l ID (string replace 'ID=' '' $ID)
+    set -l VERSION_ID (string replace 'VERSION_ID=' '' $VERSION_ID)
+
+    # Check the operating system
+    if test "$ID" = "arch" -o "$ID" = "archarm"
+        # Arch Linux or Arch ARM
+        alias hx 'helix'
+    else if test "$ID" = "ubuntu"
+        # Ubuntu
+        alias bat 'batcat'
+    end
+end
+
+# Call the function to setup the aliases
+setup_aliases
