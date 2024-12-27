52fefa74d2337643ae0613daf085d0d5d2606b59
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-12-27 11:12:09 +0800

    feat: wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm';
+local config = wezterm.config_builder()
+config = {
+    automatically_reload_config = true,
+    color_scheme = "Dracula (Official)",
+    default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" },
+    font = wezterm.font 'Fira Code',
+    font_size = 14,
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+    window_padding = { left = 16, right = 16, top = 16, bottom = 16 },
+    window_background_opacity = 0.95,
+}
+return config
