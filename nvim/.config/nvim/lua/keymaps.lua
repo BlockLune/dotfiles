242ccbd55883f4b99418cba02388966472bce323
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-07-20 01:36:43 +0800

    Add VSCode-specified keybindings

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -6,17 +6,24 @@
 vim.g.mapleader = " "
 
 -- Window Split
-if vim.g.vscode then
-    vim.keymap.set("n", "<leader>|", function()
-        require('vscode').action('workbench.action.splitEditorRight')
-    end, { desc = "Vertically Split" })
-    vim.keymap.set("n", "<leader>-", function()
-        require('vscode').action('workbench.action.splitEditorDown')
-    end, { desc = "Horizontally Split" })
-else
-    vim.keymap.set("n", "<leader>|", ":vsp<cr>", { desc = "Vertically Split" })
-    vim.keymap.set("n", "<leader>-", ":sp<cr>", { desc = "Horizontally Split" })
+local function get_split_actions()
+    if vim.g.vscode then
+        local vscode = require('vscode')
+        return function()
+            vscode.action('workbench.action.splitEditorRight')
+        end, function()
+            vscode.action('workbench.action.splitEditorDown')
+        end
+    else
+        return ":vsp<cr>", ":sp<cr>"
+    end
 end
+
+local vertical_split, horizontal_split = get_split_actions()
+
+vim.keymap.set("n", "<leader>|", vertical_split, { desc = "Vertically Split" })
+vim.keymap.set("n", "<leader>-", horizontal_split, { desc = "Horizontally Split" })
+
 
 -- Navigate
 vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "Navigate Left" })
@@ -24,11 +31,13 @@ vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "Navigate Down" })
 vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "Navigate Up" })
 vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "Navigate Right" })
 
+
 -- Resize Window
 vim.keymap.set("n", "<leader><up>", ":resize +2<cr>", { desc = "Increase Window Height" })
 vim.keymap.set("n", "<leader><down>", ":resize -2<cr>", { desc = "Decrease Window Height" })
 vim.keymap.set("n", "<leader><left>", ":vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set("n", "<leader><right>", ":vertical resize +2<cr>", { desc = "Increase Window Width" })
+
 
 -- Move Lines
 vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
@@ -38,22 +47,49 @@ vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
+
 -- Bufferline
 if vim.g.vscode then
     vim.keymap.set("n", "H", function()
         require('vscode').action('workbench.action.previousEditor')
-    end, { desc = "Previous Tab" })
+    end)
     vim.keymap.set("n", "L", function()
         require('vscode').action('workbench.action.nextEditor')
-    end, { desc = "Next Tab" })
+    end)
+    vim.keymap.set("n", "<leader>bd", function()
+        require('vscode').action('workbench.action.closeActiveEditor')
+    end)
+    vim.keymap.set("n", "<leader>bo", function()
+        require('vscode').action('workbench.action.closeOtherEditors')
+    end)
 else
     vim.keymap.set("n", "H", ":bprev<cr>", { desc = "Switch to Previous Buffer" })
     vim.keymap.set("n", "L", ":bnext<cr>", { desc = "Switch to Next Buffer" })
 end
+
 
 -- MISC
 vim.keymap.set("i", "jk", "<esc>", { desc = "Return to Normal Mode with `jk`" })
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "p", "pgvy")
 vim.keymap.set("n", "<leader>qq", ":qa<cr>", { desc = "Quit All" })
+
+
+if vim.g.vscode then
+    -- Telescope-like keymaps
+    vim.keymap.set("n", "<leader><space>", function()
+        require("vscode").action("workbench.action.quickOpen")
+    end)
+    vim.keymap.set("n", "<leader>ff", function()
+        require("vscode").action("workbench.action.quickOpen")
+    end)
+    vim.keymap.set("n", "<leader>fg", function()
+        require("vscode").action("workbench.view.search")
+    end)
+
+    -- Tree-like keymaps
+    vim.keymap.set("n", "<leader>e", function()
+        require("vscode").action("workbench.view.explorer")
+    end)
+end
 
