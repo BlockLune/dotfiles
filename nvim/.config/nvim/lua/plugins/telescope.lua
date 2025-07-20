7064b922e4f65eab48c06e314cc606f1a1605fd5
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-07-20 13:11:16 +0800

    Add telescope keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,14 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
-        { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "[Telescope] Find Files" },
-        { "<leader>ff",      "<cmd>Telescope find_files<cr>", desc = "[File] Find Files" },
-        { "<leader>fg",      "<cmd>Telescope live_grep<cr>",  desc = "[File] Live Grep" },
+        { "<leader><space>", "<cmd>Telescope find_files<cr>",  desc = "[Telescope] Find Files" },
+        { "<leader>ff",      "<cmd>Telescope find_files<cr>",  desc = "[File] Find Files" },
+        { "<leader>fg",      "<cmd>Telescope live_grep<cr>",   desc = "[File] Live Grep" },
+        { "<leader>sd",      "<cmd>Telescope diagnostics<cr>", desc = "[Show] Diagnostics" },
+        { "<leader>sj",      "<cmd>Telescope jumplist<cr>",    desc = "[Show] Jumplist" },
+        { "<leader>sk",      "<cmd>Telescope keymaps<cr>",     desc = "[Show] Keymaps" },
+        { "<leader>sl",      "<cmd>Telescope loclist<cr>",     desc = "[Show] Loclist" },
+        { "<leader>sm",      "<cmd>Telescope marks<cr>",       desc = "[Show] Marks" },
+        { "<leader>sq",      "<cmd>Telescope quickfix<cr>",    desc = "[Show] Quickfix List" },
     }
 }
