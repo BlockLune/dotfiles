d053cca0da2f002dad56cece731709380ea33fd5
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-07-20 14:54:08 +0800

    Update blinkcmp keybindings

--- nvim/.config/nvim/lua/plugins/blinkcmp.lua
+++ nvim/.config/nvim/lua/plugins/blinkcmp.lua
@@ -5,7 +5,22 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        keymap = { preset = 'enter' },
+        keymap = {
+            preset = 'enter',
+
+            ['<Tab>'] = {
+                function(cmp)
+                    if cmp.snippet_active() then
+                        return cmp.accept()
+                    else
+                        return cmp.select_and_accept()
+                    end
+                end,
+                'snippet_forward',
+                'fallback'
+            },
+            ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        },
         sources = {
             default = { "lazydev", "lsp", "path", "snippets", "buffer" },
             providers = {
