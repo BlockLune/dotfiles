bca504533bf61416f3882d31a668bd12abc86de1
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-07-26 14:47:54 +0800

    Refactor keymappings config in VSCode

--- nvim/.config/nvim/lua/keymaps/vscode.lua
+++ nvim/.config/nvim/lua/keymaps/vscode.lua
@@ -4,141 +4,76 @@
 
 local M = {}
 
-function M.setup()
-  local vscode = require("vscode")
+local function single_map(key, action, desc, mode)
+  vim.keymap.set(mode or "n", key, function()
+    require("vscode").action(action)
+  end, { desc = desc })
+end
 
+local function map(key_or_keys, action, desc, mode)
+  if type(key_or_keys) == "table" then
+    for _, key in ipairs(key_or_keys) do
+      single_map(key, action, desc, mode)
+    end
+  else
+    single_map(key_or_keys, action, desc, mode)
+  end
+end
+
+function M.setup()
   -- Window Splits
-  vim.keymap.set("n", "<leader>|", function()
-    vscode.action("workbench.action.splitEditorRight")
-  end, { desc = "Split Editor Right" })
-  vim.keymap.set("n", "<leader>-", function()
-    vscode.action("workbench.action.splitEditorDown")
-  end, { desc = "Split Editor Down" })
+  map("<leader>|", "workbench.action.splitEditorRight", "Split Editor Right")
+  map("<leader>-", "workbench.action.splitEditorDown", "Split Editor Down")
 
   -- Buffer/Tab Navigation
-  vim.keymap.set("n", "H", function()
-    vscode.action("workbench.action.previousEditor")
-  end, { desc = "Go to Previous Editor" })
-  vim.keymap.set("n", "L", function()
-    vscode.action("workbench.action.nextEditor")
-  end, { desc = "Go to Next Editor" })
-  vim.keymap.set("n", "<leader>bd", function()
-    vscode.action("workbench.action.closeActiveEditor")
-  end, { desc = "Close Current Editor" })
-  vim.keymap.set("n", "<leader>bo", function()
-    vscode.action("workbench.action.closeOtherEditors")
-  end, { desc = "Close Other Editors" })
+  map("H", "workbench.action.previousEditor", "Go to Previous Editor")
+  map("L", "workbench.action.nextEditor", "Go to Next Editor")
+  map("<leader>bd", "workbench.action.closeActiveEditor", "Close Current Editor")
+  map("<leader>bo", "workbench.action.closeOtherEditors", "Close Other Editors")
 
   -- LSP Actions
-  vim.keymap.set("n", "<leader>cr", function()
-    vscode.action("editor.action.rename")
-  end, { desc = "Rename Symbol" })
-  vim.keymap.set("n", "<leader>d", function()
-    vscode.action("editor.action.showHover")
-  end, { desc = "Show Hover Information" })
-  vim.keymap.set("n", "[d", function()
-    vscode.action("editor.action.marker.prevInFiles")
-  end, { desc = "Go to Previous Problem" })
-  vim.keymap.set("n", "]d", function()
-    vscode.action("editor.action.marker.nextInFiles")
-  end, { desc = "Go to Next Problem" })
-  -- NOTE: Unable to go to next error exactly in VSCode
-  vim.keymap.set("n", "[e", function()
-    vscode.action("editor.action.marker.prevInFiles")
-  end, { desc = "Go to Previous Problem" })
-  vim.keymap.set("n", "]e", function()
-    vscode.action("editor.action.marker.nextInFiles")
-  end, { desc = "Go to Next Problem" })
-  -- NOTE: Unable to go to next warning exactly in VSCode
-  vim.keymap.set("n", "[w", function()
-    vscode.action("editor.action.marker.prevInFiles")
-  end, { desc = "Go to Previous Problem" })
-  vim.keymap.set("n", "]w", function()
-    vscode.action("editor.action.marker.nextInFiles")
-  end, { desc = "Go to Next Problem" })
+  map("<leader>cr", "editor.action.rename", "Rename Symbol")
+  map("<leader>d", "editor.action.showHover", "Show Hover Information")
+  -- NOTE: The behaviors are different between VS Code and NeoVim
+  map({ "[d", "[e", "[w" }, "editor.action.marker.prevInFiles", "Go to Previous Problem")
+  map({ "]d", "]e", "]w" }, "editor.action.marker.nextInFiles", "Go to Next Problem")
 
   -- Code Formatting
-  vim.keymap.set("n", "<leader>cf", function()
-    vscode.action("editor.action.format")
-  end, { desc = "Format Document" })
+  map("<leader>cf", "editor.action.format", "Format Document")
 
   -- File and Search Operations
-  vim.keymap.set("n", "<leader><space>", function()
-    vscode.action("workbench.action.quickOpen")
-  end, { desc = "Quick Open Files" })
-  vim.keymap.set("n", "<leader>ff", function()
-    vscode.action("workbench.action.quickOpen")
-  end, { desc = "Find Files" })
-  vim.keymap.set("n", "<leader>/", function()
-    vscode.action("workbench.view.search")
-  end, { desc = "Find in Files" })
-  vim.keymap.set("n", "<leader>fg", function()
-    vscode.action("workbench.view.search")
-  end, { desc = "Find in Files" })
-  vim.keymap.set("n", "<leader>sd", function()
-    vscode.action("workbench.actions.view.problems")
-  end, { desc = "Show Diagnostics Panel" })
-  vim.keymap.set("n", "<leader>sl", function()
-    vscode.action("workbench.actions.view.problems")
-  end, { desc = "Show Problems List" })
-  vim.keymap.set("n", "<leader>sq", function()
-    vscode.action("workbench.actions.view.problems")
-  end, { desc = "Show Quick Fix List" })
+  map("<leader><space>", "workbench.action.quickOpen", "Quick Open Files")
+  map("<leader>ff", "workbench.action.quickOpen", "Find Files")
+  map({ "<leader>/", "<leader>fg" }, "workbench.view.search", "Find in Files")
+  map({ "<leader>D", "<leader>sd" }, "workbench.actions.view.problems", "Open Diagnostics")
+  map("<leader>sl", "workbench.actions.view.problems", "Show Problems List")
+  map("<leader>sq", "workbench.actions.view.problems", "Show Quick Fix List")
 
   -- Code Actions
-  vim.keymap.set("n", "<leader>ca", function()
-    vscode.action("editor.action.quickFix")
-  end, { desc = "Show Code Actions" })
+  map("<leader>ca", "editor.action.quickFix", "Show Code Actions")
 
   -- Todo Comments (requires gruntfuggly.todo-tree extension)
-  vim.keymap.set("n", "<leader>st", function()
-    vscode.action("workbench.view.extension.todo-tree-container")
-  end, { desc = "Show Todo Tree" })
+  map("<leader>st", "workbench.view.extension.todo-tree-container", "Show Todo Tree")
 
   -- File Explorer
-  vim.keymap.set("n", "<leader>e", function()
-    vscode.action("workbench.view.explorer")
-  end, { desc = "Toggle File Explorer" })
+  map("<leader>e", "workbench.view.explorer", "Toggle File Explorer")
 
   -- Git Integration
-  vim.keymap.set("n", "[c", function()
-    vscode.action("workbench.action.editor.previousChange")
-  end, { desc = "Go to Previous Git Change" })
-  vim.keymap.set("n", "]c", function()
-    vscode.action("workbench.action.editor.nextChange")
-  end, { desc = "Go to Next Git Change" })
-  vim.keymap.set("n", "<leader>gq", function()
-    vscode.action("workbench.view.scm")
-  end, { desc = "Show Source Control Panel" })
+  map("[c", "workbench.action.editor.previousChange", "Go to Previous Git Change")
+  map("]c", "workbench.action.editor.nextChange", "Go to Next Git Change")
+  map("<leader>gq", "workbench.view.scm", "Show Source Control Panel")
 
   -- Folding (See https://github.com/vscode-neovim/vscode-neovim/issues/58)
   -- vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = false, silent = true })
   -- vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = false, silent = true })
 
-  vim.keymap.set("n", "zM", function()
-    vscode.action("editor.foldAll")
-  end, { desc = "[Folding] Fold All" })
-  vim.keymap.set("n", "zR", function()
-    vscode.action("editor.unfoldAll")
-  end, { desc = "[Folding] Unfold All" })
-  vim.keymap.set("n", "zc", function()
-    vscode.action("editor.fold")
-  end, { desc = "[Folding] Fold" })
-  vim.keymap.set("n", "zC", function()
-    vscode.action("editor.foldRecursively")
-  end, { desc = "[Folding] Fold Recursively" })
-  vim.keymap.set("n", "zo", function()
-    vscode.action("editor.unfold")
-  end, { desc = "[Folding] Unfold" })
-  vim.keymap.set("n", "zO", function()
-    vscode.action("editor.unfoldRecursively")
-  end, { desc = "[Folding] Unfold Recursively" })
-  vim.keymap.set("n", "za", function()
-    vscode.action("editor.toggleFold")
-  end, { desc = "[Folding] Toggle Fold" })
-  vim.keymap.set("n", "<leader>z", function()
-    vscode.action("editor.toggleFold")
-  end, { desc = "[Folding] Toggle Fold" })
+  map("zM", "editor.foldAll", "[Folding] Fold All")
+  map("zR", "editor.unfoldAll", "[Folding] Unfold All")
+  map("zc", "editor.fold", "[Folding] Fold")
+  map("zC", "editor.foldRecursively", "[Folding] Fold Recursively")
+  map("zo", "editor.unfold", "[Folding] Unfold")
+  map("zO", "editor.unfoldRecursively", "[Folding] Unfold Recursively")
+  map({ "za", "<leader>z" }, "editor.toggleFold", "[Folding] Toggle Fold")
 end
 
 return M
