08c06c575ceebe6e12649c8592fd3269363331be
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-08-24 16:35:13 +0800

    Add copilot.lua

--- nvim/.config/nvim/lua/plugins/blinkcmp.lua
+++ nvim/.config/nvim/lua/plugins/blinkcmp.lua
@@ -1,6 +1,19 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { "neovim/nvim-lspconfig" },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      build = ":Copilot auth",
+      event = "BufReadPost",
+      opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      },
+    },
+    "giuxtaposition/blink-cmp-copilot",
+  },
   event = "InsertEnter",
   version = "1.*",
   ---@module 'blink.cmp'
@@ -17,18 +30,23 @@ return {
             return cmp.select_and_accept()
           end
         end,
-        "snippet_forward",
-        "fallback",
+        "snippet_forward", "fallback",
       },
       ["<S-Tab>"] = { "snippet_backward", "fallback" },
     },
     sources = {
-      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      default = { "lazydev", "lsp", "path", "snippets", "buffer", "copilot" },
       providers = {
         lazydev = {
           name = "LazyDev",
           module = "lazydev.integrations.blink",
           score_offset = 100,
+        },
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
         },
       },
     },
