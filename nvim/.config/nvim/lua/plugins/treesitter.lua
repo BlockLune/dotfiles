4174591eb41def9269c94da3465f9ef458c55d3b
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-08-30 01:24:27 +0800

    Update treesitter (master->main), and other plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
   build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local parser_config =
-      require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.wxml = {
-      install_info = {
-        url = "https://github.com/BlockLune/tree-sitter-wxml",
-        files = { "src/parser.c", "src/scanner.c" },
-        revision = "v0.2.0",
-      },
-    }
 
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      ensure_installed = {
-        "c",
-        "css",
-        "html",
-        "javascript",
-        "latex",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "rust",
-        "typst",
-        "vim",
-        "vimdoc",
-        "yaml",
-        "wxml",
-      },
-      ignore_install = {},
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-      modules = {},
+  config = function()
+    require("nvim-treesitter").install({
+      "c",
+      "css",
+      "html",
+      "javascript",
+      "latex",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "rust",
+      "typst",
+      "vim",
+      "vimdoc",
+      "yaml",
+      "wxml",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = require("nvim-treesitter").get_installed(),
+      callback = function()
+        vim.treesitter.start()
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
