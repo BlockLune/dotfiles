85d02983ce77fbdc2ade83d296abf007221084b5
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-04-29 13:00:34 +0800

    Update nvim, add auto-close when outline is last window

--- nvim/.config/nvim/lua/plugins/outline.lua
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -14,15 +14,41 @@ return {
     },
   },
   config = function(_, opts)
-    require("outline").setup(opts)
+    local outline = require("outline")
+    outline.setup(opts)
+
+    local function close_outline_if_last_window()
+      vim.schedule(function()
+        if not outline.is_open() then
+          return
+        end
+
+        local normal_windows = vim.tbl_filter(function(win)
+          return vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_config(win).relative == ""
+        end, vim.api.nvim_tabpage_list_wins(0))
+
+        if #normal_windows ~= 1 then
+          return
+        end
+
+        local buf = vim.api.nvim_win_get_buf(normal_windows[1])
+        if vim.bo[buf].filetype == "Outline" then
+          outline.close()
+        end
+      end)
+    end
+
+    vim.api.nvim_create_autocmd("WinClosed", {
+      callback = close_outline_if_last_window,
+    })
 
     local auto_opened_once = false
     vim.api.nvim_create_autocmd({ "LspAttach", "BufEnter", "FileType" }, {
       callback = function()
         if auto_opened_once then
-            return
+          return
         end
-        if require("outline").has_provider() then
+        if outline.has_provider() then
           vim.cmd("OutlineOpen!")
         end
       end,
