dc2166f7101bfe7ef5752dff85f7367c03ceb33d
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-09-13 16:13:16 +0800

    Update nvim blame.lua

--- nvim/.config/nvim/lua/plugins/blame.lua
+++ nvim/.config/nvim/lua/plugins/blame.lua
@@ -1,8 +1,31 @@
+local function theme_colors()
+  local groups = {
+    "DiagnosticInfo",
+    "DiagnosticHint",
+    "DiagnosticWarn",
+    "DiagnosticError",
+    "String",
+    "Function",
+    "Type",
+    "Keyword",
+  }
+  local colors = {}
+
+  for _, group in ipairs(groups) do
+    local fg = vim.api.nvim_get_hl(0, { name = group, link = false }).fg
+    if fg then
+      colors[#colors + 1] = string.format("#%06x", fg)
+    end
+  end
+
+  return colors
+end
+
 return {
   "FabijanZulj/blame.nvim",
   lazy = false,
   config = function()
-    require("blame").setup({})
+    require("blame").setup({ colors = theme_colors() })
   end,
   keys = {
     {
